ac8391b905ca09e0e17b0dee960c635f587e213d
LibeerDev <libeergithub@gmail.com>
2023-08-11 15:19:55 +0200

    added grid size slider, unfinished

--- script.js
+++ script.js
@@ -1,53 +1,81 @@
-const container = document.getElementById('gridContainer');
-const controls = document.getElementById('controls');
-const clear = document.getElementById('clear');
-const hideBtn = document.getElementById('hideBtn');
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('gridContainer');
+  const controlsDiv = document.getElementById('controls');
+  const clearBtn = document.getElementById('clear');
+  const hide = document.getElementById('hideBtn');
+  const sliderElement = document.getElementById('gridSlider');
+  const sliderValueElement = document.getElementById('sliderValue');
 
-let isMouseDown = false;
+  setup();
 
-// Creating the grid
-window.addEventListener('mousedown', () => {
-  isMouseDown = true;
-});
+  function setup(){
+    drawGrid(20);
+    const value = this.value;
+    sliderValueElement.textContent = value;
+  }
 
-window.addEventListener('mouseup', () => {
-  isMouseDown = false;
-});
-
-window.addEventListener('mousemove', (event) => {
-  if (isMouseDown) {
-    const square = event.target;
-    if (square.classList.contains('grid-square')) {
-      fillColor(square);
+  function drawGrid(gridSize){
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+      const square = document.createElement('div');
+      square.classList.add('grid-square');
+      container.appendChild(square);
     }
+  }
+
+  // Adding an event listener
+  sliderElement.addEventListener('input', function() {
+    const value = this.value;
+    sliderValueElement.textContent = value;
+    changeTheGridSize(value);
+  });
+  
+  function changeTheGridSize(value){
+    
+  }
+
+  let isMouseDown = false;
+  
+  
+  // Creating the grid
+  window.addEventListener('mousedown', () => {
+    isMouseDown = true;
+  });
+  
+  window.addEventListener('mouseup', () => {
+    isMouseDown = false;
+  });
+  
+  window.addEventListener('mousemove', (event) => {
+    if (isMouseDown) {
+      const square = event.target;
+      if (square.classList.contains('grid-square')) {
+        fillColor(square);
+      }
+    }
+  });
+    
+  function getAllSquares() {
+      return allGridSquares = document.querySelectorAll('.grid-square');
+  }
+  
+  function fillColor(square) {
+    square.style.backgroundColor = "red";
+  }
+  
+  clear.addEventListener('click', () => clearGrid());
+  
+  function clearGrid(){
+      getAllSquares();
+      allGridSquares.forEach((square) => square.style.backgroundColor = "white");
+  }
+  
+  hideBtn.addEventListener('click', () => hideBorder());
+  
+  function hideBorder() {
+      getAllSquares();
+      allGridSquares.forEach((square) => square.classList.toggle("hide"));
   }
 });
 
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement('div');
-  square.classList.add('grid-square');
-  container.appendChild(square);
-}
 
 
-function getAllSquares() {
-    return allGridSquares = document.querySelectorAll('.grid-square');
-}
-
-function fillColor(square) {
-  square.style.backgroundColor = "red";
-}
-
-clear.addEventListener('click', () => clearGrid());
-
-function clearGrid(){
-    getAllSquares();
-    allGridSquares.forEach((square) => square.style.backgroundColor = "white");
-}
-
-hideBtn.addEventListener('click', () => hideBorder());
-
-function hideBorder() {
-    getAllSquares();
-    allGridSquares.forEach((square) => square.classList.toggle("hide"));
-} 
